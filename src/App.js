59b6adbe2547187b7cb51b7925653fd71e4e2417
aandrews31 <101068261+aandrews31@users.noreply.github.com>
2023-01-04 20:16:32 -0500

    fixed code
    moved my weather app's HTML from the index.js to app.js, and also trimmed down unnecessary stuff in index.html

--- src/App.js
+++ src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <body>
+        <div className="container mycontainer">
+          <form id="whole-searchbar">
+            <div className="row">
+              <div className="col-10">
+                <input
+                  id="city-input"
+                  className="searchbar"
+                  type="search"
+                  placeholder="Change City..."
+                />
+              </div>
+              <div className="col-2">
+                <input type="submit" value="Search" className="btn btn-primary w-100" />
+              </div>
+            </div>
+          </form>
+          <div className="col-12">
+            <button className="btn btn-success w-25" id="current-location-button">
+              Current
+            </button>
+          </div>
+          <div className="row">
+            <div className="cityBox col">
+              <h1 id="city-name"><br /></h1>
+              <div>
+                <a id="temp"> </a><a href="#" id="celsius" className="active">°C</a>
+                <img src="" id="icon" />
+              </div>
+              <h1 id="time">Formatted Time</h1>
+              <ul>
+                <li>• <span id="description"></span></li>
+                <li>• Humidity: <span id="humidity"></span>%</li>
+                <li>• Wind: <span id="wind"></span>km/hr</li>
+              </ul>
+            </div>
+          </div>
+          <div className="weather-forecast" id="forecast"></div>
+        </div>
+        <copyright
+          ><a
+            href="https://github.com/aandrews31/Weather-app-final-linked"
+            target="_blank"
+            >Open-source Code</a
+          >
+          by Matt Delac</copyright
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+        <script src="src/script.js"></script>
+      </body>
+  </div>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -5,63 +5,6 @@ import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-function App() {
-  return (
-    <div className="App">
-      <body>
-        <div className="container mycontainer">
-          <form id="whole-searchbar">
-            <div className="row">
-              <div className="col-10">
-                <input
-                  id="city-input"
-                  className="searchbar"
-                  type="search"
-                  placeholder="Change City..."
-                />
-              </div>
-              <div className="col-2">
-                <input type="submit" value="Search" className="btn btn-primary w-100" />
-              </div>
-            </div>
-          </form>
-          <div className="col-12">
-            <button className="btn btn-success w-25" id="current-location-button">
-              Current
-            </button>
-          </div>
-          <div className="row">
-            <div className="cityBox col">
-              <h1 id="city-name"><br /></h1>
-              <div>
-                <a id="temp"> </a><a href="#" id="celsius" className="active">°C</a>
-                <img src="" id="icon" />
-              </div>
-              <h1 id="time">Formatted Time</h1>
-              <ul>
-                <li>• <span id="description"></span></li>
-                <li>• Humidity: <span id="humidity"></span>%</li>
-                <li>• Wind: <span id="wind"></span>km/hr</li>
-              </ul>
-            </div>
-          </div>
-          <div className="weather-forecast" id="forecast"></div>
-        </div>
-        <copyright
-          ><a
-            href="https://github.com/aandrews31/Weather-app-final-linked"
-            target="_blank"
-            >Open-source Code</a
-          >
-          by Matt Delac</copyright
-        >
-        <script src="src/script.js"></script>
-      </body>
-  </div>
-  );
-}
-
-
 root.render(
   <React.StrictMode>
     <App />
