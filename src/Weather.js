f5156eb843f7aa9f478551769140d86b54cbd146
aandrews31 <101068261+aandrews31@users.noreply.github.com>
2023-01-11 21:10:11 -0500

    added html back
    added back my original weather app html to my weather.js file, it is now working with the bootstrap classes

--- src/Weather.js
+++ src/Weather.js
@@ -4,8 +4,42 @@ import "./Weather.css";
 export default function Weather() {
   return (
   <div className="Weather">
-    <form form-group>Change city...</form>
-    <button className="btn btn-primary">Hello</button>
+    <form id="whole-searchbar">
+        <div class="row">
+          <div class="col-10">
+            <input
+              id="city-input"
+              class="searchbar"
+              type="search"
+              placeholder="Change City..."
+            />
+          </div>
+          <div class="col-2">
+            <input type="submit" value="Search" class="btn btn-primary w-100" />
+          </div>
+        </div>
+      </form>
+      <div class="col-12">
+        <button class="btn btn-success w-25" id="current-location-button">
+          Current
+        </button>
+      </div>
+      <div class="row">
+        <div class="cityBox col">
+          <h1 id="city-name"><br /></h1>
+          <div>
+            <a id="temp"> </a><a href="#" id="celsius" class="active">°C</a>
+            <img src="" id="icon" />
+          </div>
+          <h1 id="time">Formatted Time</h1>
+          <ul>
+            <li>• <span id="description"></span></li>
+            <li>• Humidity: <span id="humidity"></span>%</li>
+            <li>• Wind: <span id="wind"></span>km/hr</li>
+          </ul>
+        </div>
+      </div>
+      <div class="weather-forecast" id="forecast"></div>
   </div>
   )
 }
