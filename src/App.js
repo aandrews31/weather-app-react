0564e68c78c323d618441d8f6ab3525a4703f517
aandrews31 <101068261+aandrews31@users.noreply.github.com>
2023-01-11 21:06:14 -0500

    various fixes
    took out my html, fixed error linking weather.js file to app.js file, added bootstrap css link to head of index.html file

--- src/App.js
+++ src/App.js
@@ -1,60 +1,12 @@
 import './App.css';
+import Weather from './Weather';
 
-function App() {
+export default function App() {
   return (
-    <div className="App">
-      <body>
-        <div className="container mycontainer">
-          <form id="whole-searchbar">
-            <div className="row">
-              <div className="col-10">
-                <input
-                  id="city-input"
-                  className="searchbar"
-                  type="search"
-                  placeholder="Change City..."
-                />
-              </div>
-              <div className="col-2">
-                <input type="submit" value="Search" className="btn btn-primary w-100" />
-              </div>
-            </div>
-          </form>
-          <div className="col-12">
-            <button className="btn btn-success w-25" id="current-location-button">
-              Current
-            </button>
-          </div>
-          <div className="row">
-            <div className="cityBox col">
-              <h1 id="city-name"><br /></h1>
-              <div>
-                <button href="" id="temp"> </button><button href="" id="celsius" className="active">°C</button>
-                <img alt="icon" src="" id="icon" />
-              </div>
-              <h1 id="time">Formatted Time</h1>
-              <ul>
-                <li>• <span id="description"></span></li>
-                <li>• Humidity: <span id="humidity"></span>%</li>
-                <li>• Wind: <span id="wind"></span>km/hr</li>
-              </ul>
-            </div>
-          </div>
-          <div className="weather-forecast" id="forecast"></div>
-        </div>
-        <copyright
-          ><a
-            rel="noreferrer"
-            href="https://github.com/aandrews31/weather-app-react"
-            target="_blank"
-            >Open-source Code</a
-          >
-          by Matt Delac</copyright
-        >
-        <script src="src/script.js"></script>
-      </body>
+  <div className="App">
+    <div className="container">
+      <Weather />
+    </div>
   </div>
-  );
+  )
 }
-
-export default App;
---
+++ src/Weather.js
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Weather.css";
+
+export default function Weather() {
+  return (
+  <div className="Weather">
+    <form form-group>Change city...</form>
+    <button className="btn btn-primary">Hello</button>
+  </div>
+  )
+}
