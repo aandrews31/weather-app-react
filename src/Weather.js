1816f47373d3729f6bf993d730e47fbafcaeeb6e
aandrews31 <101068261+aandrews31@users.noreply.github.com>
2023-01-15 20:43:45 -0500

    various fixes, expanded on api call

--- src/Weather.js
+++ src/Weather.js
@@ -1,68 +1,74 @@
 import React, { useState } from "react";
-import "./Weather.css";
 import axios from "axios";
+import "./Weather.css";
 
 export default function Weather() {
-  const [ready, setReady] = useState(false);
-  const [temperature, setTemperature] = useState(null);
+  const [weatherData, setWeatherData] = useState({ready: false});
   function handleResponse(response) {
     console.log(response.data);
-    setTemperature(response.data.temperature.current);
-    setReady(true);
+    setWeatherData({
+      ready: true,
+      temperature: response.data.temperature.current,
+      humidity: response.data.temperature.humidity,
+      date: "Wednesday 07:00",
+      wind: response.data.wind.speed,
+      description: response.data.condition,
+      iconUrl: "http://shecodes-assets.s3.amazonaws.com/api/weather/icons/rain-night.png",
+      city: response.data.name
+    });
   }
-  if (ready) {
+
+  if (weatherData.ready) {
     return (
-      <div className="Weather">
-        <form>
-          <div className="row">
-              <div className="col-9">
-                <input
+        <div className="Weather">
+          <form>
+            <div className="row">
+                <div className="col-9">
+                  <input
                   className="form-control"
                   type="search"
                   placeholder="Change City..."
-                />
-              </div>
-              <div className="col-3">
-                <input type="submit" value="Search" className="btn btn-primary w-100" />
-              </div>
-          </div>
+                  />
+                </div>
+                <div className="col-3">
+                  <input type="submit" value="Search" className="btn btn-primary w-100" />
+                </div>
+            </div>
           </form>
           <div className="col">
             <button className="btn btn-success" id="current-location-button">
               Current
             </button>
           </div>
-          <div className="">
             <div className="cityBox">
-              <h1 id="city-name">City Name<br /></h1>
-              <div>
-                <span href="" id="temp">29</span>
-                <span href="" id="celsius">°C</span>
-                <img className="weather-icon" alt="&#9788;" src="" id="icon" />
+              <h1 id="city-name">{weatherData.city}<br /></h1>
+              <div className="currentTemp">
+                  <span href="" id="temp">{weatherData.temperature}</span>
+                  <span href="" id="celsius">°C</span>
+                  <img alt={weatherData.description} src={weatherData.iconUrl} id="icon" />
               </div>
               <h1 id="time">Formatted Time</h1>
               <ul>
-                <li>• <span id="description">Weather Description</span></li>
-                <li>• Humidity: <span id="humidity"></span>%</li>
-                <li>• Wind: <span id="wind"></span>km/hr</li>
+                <li>• <span id="description">{weatherData.description}</span></li>
+                <li>• Humidity: <span id="humidity">{weatherData.humidity}</span>%</li>
+                <li>• Wind: <span id="wind">{weatherData.wind}</span>km/hr</li>
               </ul>
             </div>
-          </div>
-          <footer><copyright
-          ><a
-            href="https://github.com/aandrews31/weather-app-react"
-            target="_blank"
-            >Open-source Code </a
-          >
-          <a class="matt-name">by Matt Delac</a></copyright
-        ></footer>
-      </div>
+            <copyright>
+              <a 
+              href="https://github.com/aandrews31/weather-app-react"
+              target="_blank"
+              >Open-source Code
+              </a>
+              <span class="matt-name">by Matt Delac</span>
+            </copyright>
+        </div>
       );
-  } else {
+    } else {
     const apiKey= "83bda23c40od4det4576797c7cafbd4d";
     let city= "London";
     let apiUrl= `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-    return "loading..."
     axios.get(apiUrl).then(handleResponse);
+    return "loading...";
   }
 }
